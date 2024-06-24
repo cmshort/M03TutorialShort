f3f7e0c4a813e1e17bd460c7522fc0645c12f741
cmshort <123396646+cmshort@users.noreply.github.com>
2024-06-24 23:13:43 +0000

    Initial commit

---
+++ global.js
@@ -0,0 +1,15 @@
+//console.log(global);
+
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname);  //abs folder path
+// console.log(__filename); //abs path + file name
+
+//console.log(document.querySelector);  //no access to this as document does not exist
